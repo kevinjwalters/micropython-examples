bfdbbcd4d129986a30d6ec49adc30a27ba9cf799
Kevin J Walters <kjw.github@gmail.com>
2021-03-27 22:22:04 +0000

    Adding used of the RGB bit for controlling all 4 NeoPixels with same colour encoding as the CircuitPython version.

--- microbit/mb-soil-moisture.py
+++ microbit/mb-soil-moisture.py
@@ -1,7 +1,8 @@
-### mb-soil-moisture.py v1.0
+### mb-soil-moisture.py v1.1
 ### Show soil moisture on micro:bit display using resistive and capacitive sensors
 
 ### Tested with BBC micro:bit v1.5 and MicroPython v1.9.2-34-gd64154c73
+### on a Cytron Edu:bit
 
 ### MIT License
 
@@ -31,20 +32,42 @@
 ### featured in
 ### https://www.instructables.com/Soil-Moisture-Sensing-With-the-Maker-Pi-Pico/
 
-import utime
-from microbit import display, pin0, pin1, sleep
 
+import utime
+from microbit import display, pin0, pin1, pin13, sleep
+import neopixel
+
+### Detach Music Bit and Sound Bit from P0 and P1 if using Edu:bit
 RES_PIN = pin0
 CAP_PIN = pin1
+
+NEOPIXEL_PIN = pin13
 
 ### Values for (dry, wet) based on values from CircuitPython version
 RES_RANGE = (1023, 156)
 CAP_RANGE = (593, 343)
 
+BLACK = (0, 0, 0)            ### black (off)
+GOOD_COLOUR = (0, 30, 0)     ### green
+DRY_COLOUR = (45, 18, 0)     ### orange
+TOODRY_COLOUR = (60, 0, 0)   ### red (used for flashing too)
+TOOWET_COLOUR = (40, 0, 40)  ### magneta
+
 ### The BBC micro:bit 5x5 LED display can show ten
 ### brightness levels in MicroPython 0-9
 ### (The underlying DAL supports a wider range 0-255)
 MAX_BRIGHTNESS = 9
+
+### The Edu:bit's RGB Bit
+NUM_PIXELS = 4
+pixels = neopixel.NeoPixel(pin13, NUM_PIXELS)
+
+
+def fill_pixels(new_colour):
+    """Set all the RGB pixels to new_colour."""
+    for idx in range(NUM_PIXELS):
+        pixels[idx] = new_colour
+    pixels.show()
 
 
 def bar_chart(value,
@@ -63,6 +86,26 @@ def bar_chart(value,
             display.set_pixel(x, y,
                               min(MAX_BRIGHTNESS, max(0, px_steps)))
         px_steps -= MAX_BRIGHTNESS
+
+
+def moisture_to_color(percents):
+    """Take multiple values and returns RGB colour and flashing boolean.
+       The smallest percentage is used for dryness colour.
+    """
+    p_colour = GOOD_COLOUR
+    p_flash = False
+    for percent in percents:
+        if percent <= 35:
+            p_colour = TOODRY_COLOUR
+        elif percent <= 45 and p_colour != TOODRY_COLOUR:
+            p_colour = DRY_COLOUR
+        elif percent >= 80:
+            p_colour = TOOWET_COLOUR
+
+        if percent <= 20:
+            p_flash = True
+
+    return (p_colour, p_flash)
 
 
 def adc_to_moisture(raw_adc, arid_value, sodden_value):
@@ -87,12 +130,14 @@ def get_cap_moisture():
     return adc_to_moisture(CAP_PIN.read_analog(), *CAP_RANGE)
 
 
+flash_toggle = False
 scroll_delay=250   ### default is 150ms
 
 while True:
     ### Read both values and display them slowly
     res_perc = get_res_moisture()
     cap_perc = get_cap_moisture()
+
     display.scroll("R " + str(round(res_perc)),
                    delay=scroll_delay)
     sleep(2000)
@@ -105,8 +150,20 @@ while True:
     start_time_ms = utime.ticks_ms()
     while True:
         cap_perc = get_cap_moisture()
+        ### Note extra brackets, one tuple parameter is passed
+        colour, flash = moisture_to_color((res_perc, cap_perc))
         bar_chart(res_perc, (0, 1))  ### Left two columns
         bar_chart(cap_perc, (3, 4))  ### Right two columns
+
+        if flash:
+            if flash_toggle:
+                fill_pixels(colour)
+            else:
+                fill_pixels(BLACK)
+            flash_toggle = not flash_toggle
+        else:
+            fill_pixels(colour)
+
         now_time_ms = utime.ticks_ms()
         if utime.ticks_diff(now_time_ms, start_time_ms) > 20000:
             break
