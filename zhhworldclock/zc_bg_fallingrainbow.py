f5d7e4afab79b1dea90c2fc75d7e1652113465e0
Kevin J Walters <kjw.github@gmail.com>
2025-09-16 22:08:25 +0100

    Enhancing FallingRainbow and RotatinRainbow backgrounds to support variable brightness.

--- zhhworldclock/zc_bg_fallingrainbow.py
+++ zhhworldclock/zc_bg_fallingrainbow.py
@@ -14,13 +14,16 @@ class FallingRainbow(HaloBackground):
         secs = self.run_time(local_time, milliseconds)
 
         ### Use symmetry of circular LED layout to set other side using same value
+        cbri = 0.80 * self.brightness
         for idx in range(len(self._zip) // 2 + 1):
             wavelength_nm = 380 + (Z_LED_POS[idx][1] + 1.0) * 150 - (secs % 60 - 30) * (380/30)
             r, g, b = wavelengthToRGBtuple(wavelength_nm)
             if wavelength_nm < 405.0:
                 b = min(b, r)  ### blue seems too high here, cap to red level
 
-            rgb_col = (int(r * 16), int(g * 16), int(b * 16))
+            rgb_col = (self.z_bri_norm(r, cbri),
+                       self.z_bri_norm(g, cbri),
+                       self.z_bri_norm(b, cbri))
             self._zip[idx] = rgb_col
             if idx != 0:
                 self._zip[len(self._zip) - idx] = rgb_col
--- zhhworldclock/zc_bg_rotatingrainbow.py
+++ zhhworldclock/zc_bg_rotatingrainbow.py
@@ -18,9 +18,12 @@ class RotatingRainbow(HaloBackground):
     def render(self, local_time, milliseconds, ticks_ms):
         secs = local_time[SECOND]
 
+        cbri = 0.80 * self.brightness
         for idx in range(len(self._zip)):
             offset_idx = (idx - secs ) % len(self._zip)
             rgb_n = wavelengthToRGBtuple(700 - 295 * offset_idx / len(self._zip))
-            self._zip[idx] = (int(rgb_n[0]*16), int(rgb_n[1]*16), int(rgb_n[2]*16))
+            self._zip[idx] = (self.z_bri_norm(rgb_n[0], cbri),
+                              self.z_bri_norm(rgb_n[1], cbri),
+                              self.z_bri_norm(rgb_n[2], cbri))
 
         return self.HALO_CHANGED
